1fce76334d1473108ff21af4b8b2105aff8721a8
Stukalov Vlad <response200@vivaldi.net>
2022-11-13 18:40:18 +0700

    Add project files.

---
+++ Calculator_WPF/MainWindow.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double number1;
+        double number2;
+        string operation = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_num_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            String number = button.Content.ToString();
+            if (text_result.Text == "0")
+            {
+                text_result.Text = number;
+            }
+            else
+            {
+                text_result.Text += number;
+            }
+
+            if (operation == "")
+            {
+                number1 = Convert.ToDouble(text_result.Text);
+            }
+            else
+            {
+                number2 = Convert.ToDouble(text_result.Text);
+            }
+        }
+
+        private void btn_operation_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            operation = button.Content.ToString();
+            text_result.Text = "0";
+        }
+
+        private void btn_eq_Click(object sender, RoutedEventArgs e)
+        {
+            double result = 0;
+            switch (operation)
+            {
+                case "+":
+                    result = number1 + number2;
+                    break;
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    break;
+                case "MIN":
+                    result = Math.Min(number1, number2);
+                    break;
+                case "MAX":
+                    result = Math.Max(number1, number2);
+                    break;
+                case "AVG":
+                    result = (number1 + number2) / 2;
+                    break;
+                case "x^y":
+                    result = GetPow(number1, (int)number2);
+                    break;
+            }
+
+            operation = "";
+            number1 = result;
+            number2 = 0;
+            text_result.Text = result.ToString();
+        }
+
+        // x^4 = x * x * x * x = x^3 * x
+        // x^3 = x * x * x = x^2 * x
+        // x^2 = x * x = x^1 * x
+        // x^1 = x = x^0 * x
+        // x^0 = 1
+        static double GetPow(double x, int y)
+        {
+            if (y == 0)
+            {
+                return 1;
+            }
+
+            return GetPow(x, y - 1) * x;
+        }
+
+        private void btn_CE_Click(object sender, RoutedEventArgs e)
+        {
+            number1 = 0;
+            number2 = 0;
+            operation = "";
+            text_result.Text = "0";
+        }
+
+        private void btn_C_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = 0;
+            }
+            else
+            {
+                number2 = 0;
+            }
+            text_result.Text = "0";
+        }
+
+        private void btn_backspase_Click(object sender, RoutedEventArgs e)
+        {
+            text_result.Text = DropLastChar(text_result.Text);
+            if (operation == "")
+            {
+                number1 = Convert.ToDouble(text_result.Text);
+            }
+            else
+            {
+                number2 = Convert.ToDouble(text_result.Text);
+            }
+
+        }
+
+        private void btn_plusminus_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 *= -1;
+                text_result.Text = number1.ToString();
+            }
+            else
+            {
+                number2 *= -1;
+                text_result.Text = number2.ToString();
+            }
+        }
+
+        private string DropLastChar(string text)
+        {
+            if (text.Length <= 1)
+            {
+                text = "0";
+            }
+            else
+            {
+                text = text.Remove(text.Length - 1, 1);
+                if (text[text.Length - 1] == ',')
+                    text = text.Remove(text.Length - 1, 1);
+            }
+            return text;
+        }
+
+        private void btn_comma_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                SetComma(number1);
+            }
+            else
+            {
+                SetComma(number2);
+            }
+        }
+
+        private void SetComma(double x)
+        {
+            if (text_result.Text.Contains(','))
+                return;
+
+            text_result.Text += ',';
+        }
+    }
+}
---
+++ Calculator_console/Program.cs
@@ -0,0 +1,146 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double numberOne;
+        double numberTwo;
+        string operation;
+
+        do
+        {
+            try
+            {
+                numberOne = GetInputNumber("Введите первое число");
+                numberTwo = GetInputNumber("Введите второе число");
+                operation = GetInputOperation("Введите арифметическую операцию -, +, *, /, max, min");
+
+                Console.WriteLine(GetResult(numberOne, numberTwo, operation));
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.Message);
+            }
+        } while (AskContinue());
+
+    }
+
+    private static bool AskContinue()
+    {
+        while (true)
+        {
+            Console.WriteLine("Хотите продолжить y/n");
+            string? answer = Console.ReadLine();
+            if (answer == "y")
+            {
+                return true;
+            }
+            else if (answer == "n")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Операция не распознана. Введите y или n: ");
+            }
+        }
+    }
+
+    static double GetInputNumber(string message)
+    {
+        Console.WriteLine(message);
+
+        while (true)
+        {
+            if (Double.TryParse(Console.ReadLine(), out double number) == true)
+            {
+                return number;
+            }
+            else
+            {
+                Console.WriteLine("Попробуйте еще раз. Введите корректное число.");
+            }
+        }
+    }
+
+    static string GetInputOperation(string message)
+    {
+        string[] operations = { "-", "+", "*", "/", "max", "min" };
+
+        Console.WriteLine(message);
+
+        while (true)
+        {
+            int indexOperation = Array.IndexOf(operations, Console.ReadLine());
+            if (indexOperation == -1)
+            {
+                Console.WriteLine(message);
+            }
+            else
+            {
+                return operations[indexOperation];
+            }
+        }
+    }
+
+    static double GetResult(double x, double y, string operation)
+    {
+        double result = 0;
+        switch (operation)
+        {
+            case "+":
+                result = x + y;
+                break;
+            case "-":
+                result = x - y;
+                break;
+            case "*":
+                result = x * y;
+                break;
+            case "/":
+                if (y == 0)
+                {
+                    throw new Exception("На 0 делить нельзя.");
+                }
+                result = x / y;
+                break;
+            case "max":
+                result = GetMax(x, y);
+                break;
+            case "min":
+                result = GetMin(x, y);
+                break;
+            default: throw new Exception("Операция не найдена");
+        }
+        return result;
+
+    }
+
+    static double GetMax(double a, double b)
+    {
+        double max = 0;
+        if (a >= b)
+        {
+            max = a;
+        }
+        else
+        {
+            max = b;
+        }
+        return max;
+    }
+
+    static double GetMin(double a, double b)
+    {
+        double min;
+        if (a <= b)
+        {
+            min = a;
+        }
+        else
+        {
+            min = b;
+        }
+        return min;
+    }
+
+}
